2f3208b23d0c4d147386532dc6df5fca21472fb3
Jude Woolls <jwoolls01@gmail.com>
2025-08-13 17:47:31 +0100

    add a basic search bar

--- MovieApp/src/pages/Home.jsx
+++ MovieApp/src/pages/Home.jsx
@@ -1,20 +1,32 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const movies = [
+    { id: 1, title: "Inception", year: 2010 },
+    { id: 2, title: "The Dark Knight", year: 2008 },
+    { id: 4, title: "Parasite", year: 2019 },
+  ];
 
-    const movies = [
-        { id: 1, title: "Inception", year: 2010 },
-        { id: 2, title: "The Dark Knight", year: 2008  },
-        { id: 4, title: "Parasite", year: 2019}
-    ]
+  const handleSearch = () => {};
 
-    return (
-        <div className="Home">
-            <div className="movies-grid">
-                {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
-            </div>
-        </div>
-    )
+  return (
+    <div className="Home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies..."
+          className="search-input"
+        />
+        <button type="submit" className="search-button">Search</button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
