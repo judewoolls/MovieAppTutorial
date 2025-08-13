bf776ebb0018ef2b0d01f6f2ab3e6a46422bbc57
Jude Woolls <jwoolls01@gmail.com>
2025-08-13 18:22:33 +0100

    add the navbar component

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -1,21 +1,21 @@
 import "./App.css";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
-
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorite />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
-
-
 
 export default App;
